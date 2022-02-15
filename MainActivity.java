ab546585e1fc2eaf0c9750ebae2a2fed7242c383
StevenMburu1 <steven.mburu@strathmore.edu>
2022-02-15 14:30:03 +0300

    Auth

--- MainActivity.java
+++ MainActivity.java
@@ -3,12 +3,24 @@ package com.example.february;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email,password;
+    TextView forgetpass;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_fab_fragment);
+
+        email= findViewById(R.id.Email);
+        password=findViewById(R.id.pass);
+        forgetpass=findViewById(R.id.forgetpass);
+        login=findViewById(R.id.button);
+
     }
 }
